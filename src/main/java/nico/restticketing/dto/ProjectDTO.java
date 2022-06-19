94b682c91066c2895236ec580c6a8c9abc77a62a
n7owhad <n7owhad@hotmail.com>
2022-06-19 06:23:22 -0400

    remaster01-4

--- src/main/java/nico/restticketing/dto/ProjectDTO.java
+++ src/main/java/nico/restticketing/dto/ProjectDTO.java
@@ -16,40 +16,15 @@ import java.time.LocalDate;
 public class ProjectDTO {
 
     private Long id;
-
-    @NotBlank
     private String projectName;
-
-    @NotBlank
     private String projectCode;
-
-    @NotNull
     private UserDTO assignedManager;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private LocalDate startDate;
-
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @NotNull
     private LocalDate endDate;
-
-    @NotBlank
     private String projectDetail;
-
     private Status projectStatus;
 
     private int completeTaskCounts;
     private int unfinishedTaskCounts;
-
-    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status projectStatus) {
-        this.projectName = projectName;
-        this.projectCode = projectCode;
-        this.assignedManager = assignedManager;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.projectDetail = projectDetail;
-        this.projectStatus = projectStatus;
-    }
 
 }
--- src/main/java/nico/restticketing/dto/TaskDTO.java
+++ src/main/java/nico/restticketing/dto/TaskDTO.java
@@ -14,30 +14,10 @@ import java.util.UUID;
 public class TaskDTO {
 
     private Long id;
-
-    @NotNull
     private ProjectDTO project;
-
-    @NotNull
     private UserDTO assignedEmployee;
-
-    @NotBlank
     private String taskSubject;
-
-    @NotBlank
     private String taskDetail;
-
     private Status taskStatus;
     private LocalDate assignedDate;
-
-    public TaskDTO(ProjectDTO project, UserDTO assignedEmployee, String taskSubject, String taskDetail, Status taskStatus, LocalDate assignedDate) {
-        this.project = project;
-        this.assignedEmployee = assignedEmployee;
-        this.taskSubject = taskSubject;
-        this.taskDetail = taskDetail;
-        this.taskStatus = taskStatus;
-        this.assignedDate = assignedDate;
-        this.id = UUID.randomUUID().getMostSignificantBits();
-    }
-
 }
