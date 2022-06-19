8fe544aa85dccb7c96e564bc812d6bb7990a3e0e
n7owhad <n7owhad@hotmail.com>
2022-06-19 05:56:14 -0400

    remaster

--- src/main/java/nico/restticketing/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package nico.restticketing.repository;
-
-import nico.restticketing.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-
-
-}
